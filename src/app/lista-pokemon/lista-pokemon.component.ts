49eb57fb82f364e73a2360485197a50f791dcff1
danigm11 <danigm821@gmail.com>
2023-11-08 16:04:52 +0100

    Selector de tipos

--- src/app/lista-pokemon/lista-pokemon.component.ts
+++ src/app/lista-pokemon/lista-pokemon.component.ts
@@ -1,17 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 import { PokemonServiceService } from '../pokemon-service.service';
 import { Pokemon } from '../model/pokemon';
-
+type tipoDeTipos={
+  name: string;
+  status: boolean;
+}
 @Component({
   selector: 'app-lista-pokemon',
   templateUrl: './lista-pokemon.component.html',
   styleUrls: ['./lista-pokemon.component.css'],
 })
+
 export class ListaPokemonComponent implements OnInit {
+
+
   listaTodo: Pokemon[] = [];
   listaMostrada: Pokemon[] = [];
   listaAux: Pokemon[] = [];
   filtroNombre: string = '';
+  listaDeTipos:tipoDeTipos[] = [
+    { name: 'bug', status: false },
+    { name: 'dark', status: false },
+    { name: 'dragon', status: false },
+    { name: 'electric', status: false },
+    { name: 'fairy', status: false },
+    { name: 'fighting', status: false },
+    { name: 'fire', status: false },
+    { name: 'flying', status: false },
+    { name: 'ghost', status: false },
+    { name: 'grass', status: false },
+    { name: 'ground', status: false },
+    { name: 'ice', status: false },
+    { name: 'normal', status: false },
+    { name: 'poison', status: false },
+    { name: 'psychic', status: false },
+    { name: 'rock', status: false },
+    { name: 'steel', status: false },
+    { name: 'water', status: false }
+  ];
+  filtroTipo: string[]= [];
   numeroInicial: number = 1;
   numeroPokemon: number = 151;
   ultimoPulsado: number =1;
@@ -19,6 +46,7 @@ export class ListaPokemonComponent implements OnInit {
   constructor(private pokemonService: PokemonServiceService) {}
   ngOnInit(): void {
     this.cargaPokemon();
+    console.log(this.filtroTipo)
   }
   cargaPokemon() {
     this.pokemonService.getPokemons().subscribe((pokemons: Pokemon[]) => {
@@ -29,9 +57,15 @@ export class ListaPokemonComponent implements OnInit {
       this.listaTodo = pokemons;
     });
   }
+  aplicarFiltros(n:number){
+
+    this.filtrarGen(n);
+    this.filtrarTipo();
+    this.filtrarNombre();
+  }
 
   filtrarNombre() {
-    this.listaMostrada = this.listaAux.filter((Pokemon) =>
+    this.listaMostrada = this.listaMostrada.filter((Pokemon) =>
       Pokemon.nombre.includes(this.filtroNombre.toLowerCase())
     );
   }
@@ -68,5 +102,24 @@ export class ListaPokemonComponent implements OnInit {
     );
     this.listaAux = this.listaMostrada;
     this.ultimoPulsado = n;
+  }
+  filtrarTipo(){
+    this.filtroTipo=[];
+    for (let i = 0; i < 18; i++) {
+      if(this.listaDeTipos[i].status){
+        this.filtroTipo.push(this.listaDeTipos[i].name)
+      }
+    }
+    if(this.filtroTipo.length<1){
+      for (let i = 0; i < 18; i++) {
+        this.filtroTipo.push(this.listaDeTipos[i].name)
+        }
+    }
+    this.listaMostrada = this.listaAux.filter((Pokemon) =>
+    this.filtroTipo.includes(Pokemon.tipos[0])
+  );
+  
+
+    this.listaAux = this.listaMostrada;
   }
 }
