bda04de98538c268f526e4ed0eb26b39acfc58f9
PabloJavier22 <pmungar408@g.educaand.es>
2023-11-08 16:06:48 +0100

    todo ya ta listo
    primera visita

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { ListaPokemonComponent } from './lista-pokemon/lista-pokemon.component';
 import { FormsModule } from '@angular/forms';
 import { BotonVolverArribaComponent } from './boton-volver-arriba/boton-volver-arriba.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
 declarations: [
 AppComponent,
 ListaPokemonComponent,
-BotonVolverArribaComponent
+BotonVolverArribaComponent,
+HeaderComponent
 ],
 imports: [
 BrowserModule,
