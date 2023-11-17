f1125dad3645abbbf7128593d4a0e07e7245a50a
PabloJavier22 <pmungar408@g.educaand.es>
2023-11-17 17:07:04 +0100

    Ya lo arregle
    Esta hecho cutre pero funciona

--- src/app/pokemon-detail/pokemon-detail.component.ts
+++ src/app/pokemon-detail/pokemon-detail.component.ts
@@ -7,43 +7,66 @@ import { Observable, map } from 'rxjs';
 @Component({
   selector: 'app-pokemon-detail',
   templateUrl: './pokemon-detail.component.html',
-  styleUrls: ['./pokemon-detail.component.css']
+  styleUrls: ['./pokemon-detail.component.css'],
 })
 export class PokemonDetailComponent {
-  
-  id: number=0;
-  listaX4:String[]=[];
-  listaX2:String[]=[];
-  listaX1:String[]=[];
-  listaX12:String[]=[];
-  listaX14:String[]=[];
-  listaX0:String[]=[];
-  listaX2Aux:String[]=[];
-  listaX1Aux:String[]=[];
-  listaX12Aux:String[]=[];
-  listaX0Aux:String[]=[];
+  id: number = 0;
+  listaX4: String[] = [];
+  listaX2: String[] = [];
+  listaX1: String[] = [];
+  listaX12: String[] = [];
+  listaX14: String[] = [];
+  listaX0: String[] = [];
+  listaX2Aux: String[] = [];
+  listaX1Aux: String[] = [];
+  listaX12Aux: String[] = [];
+  listaX0Aux: String[] = [];
   ngOnInit(): void {
-    this.id = this.activatedRoute.snapshot.paramMap.get('id') as unknown as number;
+    this.id = this.activatedRoute.snapshot.paramMap.get(
+      'id'
+    ) as unknown as number;
     this.cargaPokemon();
   }
   detalle: any;
-  imagenActual: string = "";
-  constructor(private pokemonService: PokemonServiceService, private activatedRoute: ActivatedRoute,private http: HttpClient) {
+  imagenActual: string = '';
+  constructor(
+    private pokemonService: PokemonServiceService,
+    private activatedRoute: ActivatedRoute,
+    private http: HttpClient
+  ) {}
+
+  async cargaPokemon() {
+    try {
+      const pokemons: PokemonDetail | undefined = await this.pokemonService
+        .getDetalles(this.id)
+        .toPromise();
+
+      if (pokemons) {
+        this.detalle = pokemons;
+        this.imagenActual = this.detalle.imagen;
+
+        await this.cargarListasX(pokemons.tipos[0], 2);
+        await this.cargarListasX(pokemons.tipos[0], 0);
+        await this.cargarListasX(pokemons.tipos[0], 0.5);
+
+        if (pokemons.tipos[1]) {
+          await this.cargarListasX(pokemons.tipos[1], 2);
+          await this.cargarListasX(pokemons.tipos[1], 0);
+          await this.cargarListasX(pokemons.tipos[1], 0.5);
+        }
+      } else {
+        console.error('Pokemon details not found');
+      }
+    } catch (error) {
+      console.error('Error loading Pokemon details:', error);
+    }
   }
-  cargaPokemon(){
-    this.pokemonService.getDetalles(this.id).subscribe((pokemons: PokemonDetail) => {
-      this.detalle=pokemons;this.imagenActual= this.detalle.imagen;
-      this.cargarListasX(pokemons.tipos[0],2);
-      this.cargarListasX(pokemons.tipos[0],0);this.cargarListasX(pokemons.tipos[0],0.5);
-      if(pokemons.tipos[1]){
-        this.cargarListasX(pokemons.tipos[1],2);
-        this.cargarListasX(pokemons.tipos[1],0);this.cargarListasX(pokemons.tipos[1],0.5);
-      }}
-      );
-  }
-  
+
   cambiarImagen() {
-    this.imagenActual = this.imagenActual === this.detalle.imagen ? this.detalle.imagenShiny : this.detalle.imagen;
+    this.imagenActual =
+      this.imagenActual === this.detalle.imagen
+        ? this.detalle.imagenShiny
+        : this.detalle.imagen;
   }
 
   getJsonData(): Observable<any> {
@@ -51,14 +74,16 @@ export class PokemonDetailComponent {
   }
   returnJsonData(tipo: string, efectividad: number): Observable<String[]> {
     return this.getJsonData().pipe(
-      map(data => {
+      map((data) => {
         return data[tipo][efectividad] as String[];
       })
     );
   }
-  cargarListasX(tipo:string,n:number){
-    this.returnJsonData(tipo, n).subscribe(
-      data => {
+  async cargarListasX(tipo: string, n: number) {
+    try {
+      const data = await this.returnJsonData(tipo, n).toPromise();
+
+      if (data !== undefined) {
         if (n == 2) {
           this.listaX2Aux = this.listaX2.slice();
           this.listaX2 = data;
@@ -68,39 +93,54 @@ export class PokemonDetailComponent {
         } else if (n == 0.5) {
           this.listaX12Aux = this.listaX12.slice();
           this.listaX12 = data;
-          if(tipo=this.detalle.tipos[1]){
+          if (tipo === this.detalle.tipos[1]) {
             this.ordenarListasX();
           }
         }
       }
-    );
+    } catch (error) {
+      console.error('Error loading data:', error);
+    }
   }
-  ordenarListasX(){
 
-    this.listaX0Aux.forEach(elemento => {
+  ordenarListasX() {
+    this.listaX0Aux.forEach((elemento) => {
       if (!this.listaX0.includes(elemento)) {
-          this.listaX0.push(elemento);
+        this.listaX0.push(elemento);
       }
     });
-    this.listaX4 = this.listaX2.filter(item => this.listaX2Aux.includes(item));
-    this.listaX2Aux.forEach(elemento => {
+    this.listaX4 = this.listaX2.filter((item) =>
+      this.listaX2Aux.includes(item)
+    );
+    this.listaX2Aux.forEach((elemento) => {
       if (!this.listaX2.includes(elemento)) {
-          this.listaX2.push(elemento);
+        this.listaX2.push(elemento);
       }
     });
-    this.listaX14 = this.listaX12.filter(item => this.listaX12Aux.includes(item));
-    this.listaX12Aux.forEach(elemento => {
+    this.listaX14 = this.listaX12.filter((item) =>
+      this.listaX12Aux.includes(item)
+    );
+    this.listaX12Aux.forEach((elemento) => {
       if (!this.listaX12.includes(elemento)) {
-          this.listaX12.push(elemento);
+        this.listaX12.push(elemento);
       }
     });
-    this.listaX2 = this.listaX2.filter(item => !this.listaX4.includes(item));
-    this.listaX12 = this.listaX12.filter(item => !this.listaX14.includes(item));
-    this.listaX4 = this.listaX4.filter(item => !this.listaX0.includes(item));
-    this.listaX2 = this.listaX2.filter(item => !this.listaX0.includes(item));
-    this.listaX12 = this.listaX12.filter(item => !this.listaX0.includes(item));
-    let listaX2copia:String[]=this.listaX2;
-    this.listaX2 = this.listaX2.filter(elemento => !this.listaX12.includes(elemento));
-    this.listaX12 = this.listaX12.filter(elemento => !listaX2copia.includes(elemento));
+    this.listaX2 = this.listaX2.filter((item) => !this.listaX4.includes(item));
+    this.listaX12 = this.listaX12.filter(
+      (item) => !this.listaX14.includes(item)
+    );
+    this.listaX4 = this.listaX4.filter((item) => !this.listaX0.includes(item));
+    this.listaX2 = this.listaX2.filter((item) => !this.listaX0.includes(item));
+
+    this.listaX12 = this.listaX12.filter(
+      (item) => !this.listaX0.includes(item)
+    );
+    let listaX2copia: String[] = this.listaX2;
+    this.listaX2 = this.listaX2.filter(
+      (elemento) => !this.listaX12.includes(elemento)
+    );
+    this.listaX12 = this.listaX12.filter(
+      (elemento) => !listaX2copia.includes(elemento)
+    );
   }
 }
