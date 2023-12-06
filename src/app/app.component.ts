89442ec009b11df06cc98a4effa1c1c01ba48d2b
PabloJavier22 <117372943+PabloJavier22@users.noreply.github.com>
2023-12-06 19:50:19 +0100

    Modo Oscuro On Point

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  ngOnInit(): void {
+    this.setTheme()
+  }
 
+setTheme(): void {
+const prefersLight = window.matchMedia('(prefers-color-scheme: light)').matches;
+
+if (localStorage.getItem('theme') == 'light') {
+
+  document.body.classList.toggle('dark-theme', !prefersLight);
+
+} else {
+  document.body.classList.toggle('light-theme', prefersLight);
+
+}
+}
 }
