0d74498dfd9c075060a77d46f8318e81e9253011
danigm11 <danigm821@gmail.com>
2023-12-09 23:32:01 +0100

    Texto tipos modo claro

--- src/app/model/pokemon-cadena.ts
+++ src/app/model/pokemon-cadena.ts
@@ -1,13 +1,8 @@
 import { Pokemon } from "./pokemon";
-import { Trigger } from "./trigger";
-/*
-export interface Evolution {
-pokemons: Pokemon[];
-triggers?: Trigger[];
-}*/
+
 export interface Evolution {
   pokemon: Pokemon[];
   evo1: Pokemon[];
   evo2: Pokemon[];
   triggers: string[][];
-  }/**/
+  }
